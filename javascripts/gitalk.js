27059de586110142cd153a751641ff61aed52cc2
cornorghost <1648313898@qq.com>
2020-07-21 21:39:49 +0800

    Site updated: 2020-07-21 21:39:48

--- javascripts/gitalk.js
+++ javascripts/gitalk.js
@@ -1,14 +1,6 @@
 ;(function () {
     if (window.aomori && window.aomori.gitalk) {
-        const gitalk = new Gitalk({
-            clientID: 'f481f237218c36c2740b',
-            clientSecret: '6d0738e25fd81401b6505e7a1364859ab523a716',
-            repo: 'blogcomments',
-            owner: 'cornorghost',
-            admin: ['cornorghost'],
-            id: '1',
-            distractionFreeMode: false,
-        })
+        const gitalk = new Gitalk(window.aomori.gitalk)
         gitalk.render('gitalk-container')
     }
 })()
